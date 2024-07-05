9c3f2743529ab3026624204f2a5cf124cc5cb1f0
Shoxrux061 <shoxruxurmanov2@gmail.com>
2024-07-05 17:10:31 +0500

    change api

--- app/src/main/java/uz/isystem/weatherappcompose/utills/Constants.kt
+++ app/src/main/java/uz/isystem/weatherappcompose/utills/Constants.kt
@@ -1,6 +1,6 @@
 package uz.isystem.weatherappcompose.utills
 
 object Constants {
-    const val BASE_URL = "http://api.weatherapi.com/v1"
-    const val  API_KEY = "417665f7d0b24fe29a6104457240507"
+    const val BASE_URL = "https://api.weatherbit.io/v2.0/"
+    const val API_KEY = "856a1a62807547d9a96f638874fffb9c"
 }
