87304fafa1c0d2f5314441ca6b35794e033aeaf0
Shoxrux061 <shoxruxurmanov2@gmail.com>
2024-07-05 16:57:00 +0500

    Initial commit

---
+++ app/src/main/java/uz/isystem/weatherappcompose/utills/Constants.kt
@@ -0,0 +1,6 @@
+package uz.isystem.weatherappcompose.utills
+
+object Constants {
+    const val BASE_URL = "http://api.weatherapi.com/v1"
+    const val  API_KEY = "417665f7d0b24fe29a6104457240507"
+}
